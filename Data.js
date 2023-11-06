66734fd031e57a3b5cb43d143d79b8b790b9da4f
sumanthsai492 <67100492+sumanthsai492@users.noreply.github.com>
2023-11-06 22:58:58 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,41 @@
+let Students = [];
+const StudentForm = document.getElementById("StudentForm");
+const StudentTable = document.getElementById("StudentTable");
+
+function StudentData(event) {
+  event.preventDefault();
+  const StudentName = document.getElementById("Name").value;
+  const StudentAge = document.getElementById("Age").value;
+  const StudentGrade = document.getElementById("Grade").value;
+  const StudentCity = document.getElementById("City").value;
+  const Student = [StudentName, StudentAge, StudentGrade, StudentCity];
+  Students.push(Student);
+  StudentForm.reset();
+  updateTable();
+}
+
+function updateTable() {
+  StudentTable.innerHTML = "";
+
+  Students.forEach((Student) => {
+    const row = document.createElement("tr");
+    const nameCell = document.createElement("td");
+    const ageCell = document.createElement("td");
+    const gradeCell = document.createElement("td");
+    const cityCell = document.createElement("td");
+
+    nameCell.textContent = Student[0];
+    ageCell.textContent = Student[1];
+    gradeCell.textContent = Student[2];
+    cityCell.textContent = Student[3];
+
+    row.appendChild(nameCell);
+    row.appendChild(ageCell);
+    row.appendChild(gradeCell);
+    row.appendChild(cityCell);
+
+    StudentTable.appendChild(row);
+  });
+}
+
+StudentForm.addEventListener("submit", StudentData);
